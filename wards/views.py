af460d4e883ae8c3dc0246c5ad7b41cf9ed560ec
Nickghost inc <you@example.com>
2018-10-11 08:23:20 +0300

    first Template created

--- wards/views.py
+++ wards/views.py
@@ -1,5 +1,15 @@
 from django.http  import HttpResponse
+from django.shortcuts import render,redirect,get_object_or_404
 
 # Create your views here.
-def welcome(request):
-    return HttpResponse('Welcome to the Awwwards')
+
+# @login_required(login_url='/login/')
+def awards(request):
+    post = Image.objects.all()
+    return render(request,'awards.html',{"post":post})
+
+def pics(request):
+    pictures = Image.objects.all()
+
+    return render(request, "awards.html", {"pictures": pictures})
+
